6fcf8ac662faf40f01515064084667e468f44eb3
Daisy <hernandezvegadaisy@gmail.com>
2025-09-09 21:36:49 -0500

    Cotizador

---
+++ app/cotizador/src/main/java/com/arkahv/cotizador/infraestructure/adapter/out/persistence/repository/ProductoRepository.java
@@ -0,0 +1,10 @@
+package com.arkahv.cotizador.infraestructure.adapter.out.persistence.repository;
+
+import com.arkahv.cotizador.infraestructure.adapter.out.persistence.entity.ProductoEntity;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import reactor.core.publisher.Flux;
+
+public interface ProductoRepository extends ReactiveCrudRepository<ProductoEntity, Integer> {
+
+    Flux<ProductoEntity> findByIdCotizacion(Integer idCotizacion);
+}
